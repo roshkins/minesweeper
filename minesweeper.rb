6bd15d83e49bb405bd26edb16d7485199cd9bc5d
Rashi + Flarnie <student@appacademy.io>
2013-07-22 11:39:36 -0700

    Added self.build_board to Board class.

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,9 +4,20 @@ class Board
     opts   = opts.merge( {size: 9, mines: 10} )
     @size  = opts[:size]
     @mines = opts[:mines]
+    @board = self.build_board(@size)
   end
 
-
+  def self.build_board(size)
+    board = []
+    size.times do
+      line = []
+      board << line
+      size.times do
+        line << nil
+      end
+    end
+    board
+  end
 
 end
 
